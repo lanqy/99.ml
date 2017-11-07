2fdc538ca0ed9e17e04366c1e306482bdf46eb2e
Lan Qingyong <lanqy1314@gmail.com>
2017-11-07 18:04:29 +0800

    Update p09.ml

--- p09.ml
+++ p09.ml
@@ -5,8 +5,9 @@ let pack list =
     | [] -> [] (* Can only be reached if original list is empty *)
     | [x] -> (x :: current) :: acc
     | a :: (b :: _ as t) ->
-      if a = b then aux (a :: current) acc t
-      else aux [] ((a :: current) :: acc) t in
+      if a = b then aux (a :: current) acc t else aux [] ((a :: current) :: acc) t
+    in
+    
     List.rev (aux [] [] list)
 ;;
 
