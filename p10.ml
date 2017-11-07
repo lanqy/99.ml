7dc516c6e2152d20fed0471bcf50c8b2c02f297e
Lan Qingyong <lanqy1314@gmail.com>
2017-11-07 18:05:02 +0800

    Update p10.ml

--- p10.ml
+++ p10.ml
@@ -3,8 +3,9 @@ let encode list =
   let rec aux count acc = function
     | [] -> [] (* Can only be reached if original list is empty *)
     | [x] -> (count + 1, x) :: acc
-    | a :: (b :: _ as t) -> if a = b then aux (count + 1) acc t
-                            else aux 0 ((count + 1, a) :: acc) t in
+    | a :: (b :: _ as t) -> if a = b then aux (count + 1) acc t else aux 0 ((count + 1, a) :: acc) t
+  in
+  
   List.rev (aux 0 [] list);;
 
 (* let encode list =
