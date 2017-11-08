c8da800b461f17b1b731b43817c5f82579ea8dd5
Lan Qingyong <lanqy1314@gmail.com>
2017-11-08 18:16:18 +0800

    Add files via upload

---
+++ p16.ml
@@ -0,0 +1,8 @@
+(* Drop every N'th element from a list. *)
+let drop list n =
+  let rec aux i = function
+    | [] -> []
+    | h :: t -> if i = n then aux 1 t else h :: aux (i + 1) t in
+  aux 1 list;;
+
+drop ["a";"b";"c";"d";"e";"f";"g";"h";"i";"j"] 3;;
