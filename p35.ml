bc50d787f0a588563fb32dd28253b5e37f45cc87
Lan Qingyong <lanqy1314@gmail.com>
2017-11-21 20:41:42 +0800

    Add files via upload

---
+++ p35.ml
@@ -0,0 +1,12 @@
+(* Determine the prime factors of a given positive integer. *)
+
+(* Recall that d divides n iff [n mod d = 0] *)
+
+let factors n =
+  let rec aux d n =
+    if n = 1 then [] else
+      if n mod d = 0 then d :: aux d (n / d) else aux (d + 1) n
+  in
+  aux 2 n;;
+
+factors 315;;
---
+++ p36.ml
@@ -0,0 +1,13 @@
+(* Determine the prime factors of a given positive integer (2) *)
+let factors n =
+  let rec aux d n =
+    if n = 1 then [] else
+      if n mod d = 0 then
+        match aux d (n / d) with
+        | (h, n) :: t when h = d -> (h, n + 1) :: t
+        | l -> (d, 1) :: l
+      else aux (d + 1) n
+  in
+  aux 2 n;;
+
+factors 315;;
