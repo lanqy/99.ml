7a3f4792ae8fa4772b04f0b69eca428a8b75703f
Lan Qingyong <lanqy1314@gmail.com>
2017-11-22 14:04:19 +0800

    Add files via upload

---
+++ p39.ml
@@ -0,0 +1,14 @@
+(* A list of prime numbers. *)
+
+let is_prime n =
+  let n = max n (-n) in
+  let rec is_not_divisor d =
+    d * d > n || (n mod d <> 0 && is_not_divisor (d + 1)) in
+  is_not_divisor 2
+
+let rec all_primes a b =
+  if a > b then [] else
+    let rest = all_primes (a + 1) b in
+    if is_prime a then a :: rest else rest;;
+
+List.length (all_primes 2 7920);;
