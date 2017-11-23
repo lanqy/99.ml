03e78b4ff56ccde75a60c060e3c35a31a9e51181
Lan Qingyong <lanqy1314@gmail.com>
2017-11-23 08:42:26 +0800

    Add files via upload

---
+++ p40.ml
@@ -0,0 +1,9 @@
+(* Goldbach's conjecture.  *)
+(* [is_prime] is defined in the previous solution *)
+let goldbach n =
+  let rec aux d =
+    if is_prime d && is_prime (n - d) then (d, n - d)
+    else aux (d + 1) in
+  aux 2;;
+
+goldbach 28;;
